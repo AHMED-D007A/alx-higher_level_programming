1953b58ba8b9a23bec8e2c0854477ea923ecfd58
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-12 01:22:40 +0000

    fixing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if length <= 1:
         print("0 arguments.")
     else:
+        if length == 2:
         print("{:d} argument:".format(length - 1))
+        else:
+        print("{:d} arguments:".format(length - 1))
         for i in range(1, length):
             print("{:d}: {}".format(i, sys.argv[i]))
