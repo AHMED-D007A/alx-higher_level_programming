fe6da073bd3a70820ecb83faf00aa67ba9be3dcc
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-15 15:56:14 -0700

    no.4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (0 <= idx < len(my_list)):
+        cp_my_list = my_list
+        cp_my_list.remove(cp_my_list[idx])
+        cp_my_list.insert(idx, element)
+        return (cp_my_list)
+    else:
+        return (my_list)
