79644adf29730679c428681124b80d9d699fd170
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-15 16:00:56 -0700

    no.4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (0 <= idx < len(my_list)):
-        cp_my_list = my_list
+    if my_list is None:
+        return None
+    elif (0 <= idx < len(my_list)):
+        cp_my_list = my_list[:]
         cp_my_list.remove(cp_my_list[idx])
         cp_my_list.insert(idx, element)
         return (cp_my_list)
