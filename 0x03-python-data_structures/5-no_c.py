ce4e1d1628ac2d411088df95ffe68dc5c0fc0827
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-15 16:18:26 -0700

    no.5 fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for ch in my_string:
-        if (ch == 'c' or ch == 'C'):
-            my_string.remove(ch)
+    return my_string.translate({ord(c): None for c in "cC"})
