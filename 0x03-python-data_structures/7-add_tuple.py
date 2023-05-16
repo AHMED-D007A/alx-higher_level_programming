0559e976fb22b4b1ff0cd8c54ca0687e678a3a55
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-15 17:12:33 -0700

    fixing

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    check_a = 2 - tuple_a
-    check_b = 2 - tuple_b
-    if (check_a):
-        tuple_a += (0) * check_a
-    if (check_b):
-        tuple_b += (0) * check_b
-    tuple_sum = tuple(({x + y for x, y in zip(tuple_a, tuple_b)}))
-    return (tuple_sum[0:2])
+    first = list(tuple_a) + [0]*2
+    sec = list(tuple_b) + [0]*2
+    sum = [x + y for x, y in zip(first, sec)]
+    return tuple(sum[0:2])
