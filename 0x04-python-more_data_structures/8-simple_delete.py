f8db5ca9569de0d892a63241d06fd3c236664cc1
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-19 18:31:34 -0700

    no.8 fixed

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key == list(a_dictionary.keys()):
-        del a_dictionary[key]
+    a_dictionary.pop(key, None)
     return a_dictionary
