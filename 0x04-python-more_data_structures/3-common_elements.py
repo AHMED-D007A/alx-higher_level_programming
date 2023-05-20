778c360377c8cbbaae7a424caada6de65f0dd5b4
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-19 17:01:31 -0700

    fixing'

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     out = []
-    for i in range(set_1):
+    for i in range(0, set_1):
         if (set_1[i] == set_2[i]):
             out += set_1[i]
     return (out)
