fcecb1b7785f35b2ef4943010daef7ad3e2e7455
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-30 19:49:40 -0700

    fixing codestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-this is a "Square" module.
+This is the "Square"  module.
 
-it provides a Square class with initialization of size.
-Size defaults to 0. Raise errors in invalid inputs.
+This module provides a simple Square class with initialize size.
+Size defaults to 0. Raise errors on invalid inputs.
 """
 
 
 class Square:
-    """a simple Square module wiht initialization of size(int >= 0)"""
-    def __init__(self, size = 0):
+    """A class that defines a square by size"""
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
