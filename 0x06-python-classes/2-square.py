4846f4d3e50c90c4ae98d9f74324fe8f8c358595
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-05-30 19:44:21 -0700

    fixing codestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,6 +4,8 @@ this is a "Square" module.
 
 it provides a Square class with initialization of size.
 """
+
+
 class Square:
     """a simple Square module wiht initialization of size(int >= 0)"""
     def __init__(self, size = 0):
