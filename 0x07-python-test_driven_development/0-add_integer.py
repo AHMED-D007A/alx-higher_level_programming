91254eedd4fb4c2b7c6adc288075248a809c13ae
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-06-16 05:52:46 -0700

    no.0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,8 @@ it add two numbers and return them to the user.
 def add_integer(a, b=98):
     """this module add two integers or floats and return them"""
     my_list = list(map(lambda x: isinstance(x, (int, float)), [a, b]))
-
+    if all(my_list):
+        return int(a) + int(b)
     for x, y in list(zip(my_list, ['a', 'b'])):
         if not x:
             raise TypeError("{} must be an integer".format(y))
-
-    return int(a) + int(b)
