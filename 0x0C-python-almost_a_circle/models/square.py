c2e61ce593d7117aa60a4ed7da24491e5a06e1bb
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-06-19 09:53:10 -0700

    fixing

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,13 +13,12 @@ class Square(Rectangle):
         if size <= 0:
             raise ValueError("{} must be > 0".format("size"))
         super().__init__(size, size, x, y, id)
-        self.__size = size
 
     @property
     def size(self):
         """methon to property gives the size
         Return: the size of the square"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, size):
@@ -27,29 +26,28 @@ class Square(Rectangle):
             raise TypeError("{} must be an integer".format("width"))
         if size <= 0:
             raise ValueError("{} must be > 0".format("width"))
-        self.__size = size
         self.width = size
         self.height = size
 
     def __str__(self):
         """a method to get info about the clss of instance
         Return: humain readable info"""
-        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.__size}")
+        return (f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y} - {self.width}")
 
     def update(self, *args, **kwargs):
         """a method to update the attributes of the calss
         It's order goes as the following:
             1- id
-            2- width
-            3- height
-            4- x
-            5- y
+            2- size
+            3- x
+            4- y
             """
         for i in range(len(args)):
             if i == 0:
                 self.id = args[i]
             if i == 1:
                 self.width = args[i]
+                self.height = args[i]
                 self.__size = args[i]
             if i == 2:
                 self.x = args[i]
@@ -58,8 +56,9 @@ class Square(Rectangle):
         for k, v in kwargs.items():
             if k == "id":
                 self.id = v
-            if k == "size":
+            if k == "size" or k == "width" or k == "height":
                 self.width = v
+                self.height = v
                 self.__size = v
             if k == "x":
                 self.x = v
@@ -71,7 +70,7 @@ class Square(Rectangle):
         Return: the dic of its attributes"""
         return {
             "id" : self.id,
+            "size" : self.width,
             "x" : self.x,
-            "size" : self.__size,
             "y" : self.y
         }
