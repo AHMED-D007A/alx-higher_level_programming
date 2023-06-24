25b92ec49f0abedcf091069f9977401c182b7d83
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-06-24 13:34:20 +0300

    task no.1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is the Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class that has private width and hight attributes"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height= height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, val):
+        if type(val) is not int:
+            raise TypeError('width must be an integer')
+        if val < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = val
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, val):
+        if type(val) is not int:
+            raise TypeError('height must be an integer')
+        if val < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = val
