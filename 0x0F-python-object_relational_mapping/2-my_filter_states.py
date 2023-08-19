131842482051720f95171ab5839c9b24253031fe
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-08-19 21:20:29 +0300

    fixing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                            passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' \
+    cur.execute("SELECT * FROM states WHERE name='%{:s}%' \
                 ORDER BY id ASC;".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     sp = sys.argv[4].split("'")
     name = sp[0]
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY id ASC;", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name='{}'\
+                ORDER BY id ASC;".format(name))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
