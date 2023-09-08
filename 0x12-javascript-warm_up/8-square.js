36369f1d82fae43ac0a52c51574f7406fd970c4a
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-09-08 00:32:20 -0700

    task no.7, no.8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const len = process.argv[2];
+if (!process.argv[2]) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < len; i++) {
+    console.log('X'.repeat(len));
+  }
+}
