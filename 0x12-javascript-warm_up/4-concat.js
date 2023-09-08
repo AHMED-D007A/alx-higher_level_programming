fa55c9a37c41700fb025296ed5a88e5433723147
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-09-07 23:34:34 -0700

    task no.5

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const argv = process.argv;
-
-const args = [];
-args.push(argv[2]);
-args.push(argv[3]);
-const str = args.join(' is ');
-console.log(str);
+console.log(argv[2] + ' is ' + argv[3]);
